5e4a977a72d97eeddeb3f029067381c4c11b9ce6
= <=>
2025-11-13 08:18:44 +0700

    them animation duonng dua xe chay cho duong dua

--- public/demos/race-mini-game/race.js
+++ public/demos/race-mini-game/race.js
@@ -2,7 +2,16 @@ const raceBtnElement = document.querySelector('.race-button');
 const playerElement = document.querySelector('.player');
 const enemy1 = document.querySelector('.enemy1');
 const enemy2 = document.querySelector('.enemy2');
-
+const line1Element = document.querySelector('.center-line1');
+const line2Element = document.querySelector('.center-line2');
+const finishLineElement = document.querySelector('.finish-line');
+const raceTrackElement = document.querySelector('.race-track');
+const message = document.querySelector('.message')
+const playerWidth = playerElement.clientWidth;
+const trackWidth = raceTrackElement.clientWidth;
+let currentPosition = 0;
+let gameStarted = false
+let gameWon = false;
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -11,6 +20,15 @@ function getRandomInt(min, max) {
 }
 
 
+
+function startRaceLine() {
+  line1Element.classList.add('running');
+  line2Element.classList.add('running');
+}
+function stopRaceLine() {
+  line1Element.classList.remove('running');
+  line2Element.classList.remove('running');
+}
 function moveOpponent(enemy, currentPosition) {
   if (gameWon) {
     return;
@@ -19,6 +37,7 @@ function moveOpponent(enemy, currentPosition) {
   currentPosition += randomStep;
   enemy.style.transform = `translateX(${currentPosition}px)`;
   if (10 + currentPosition + enemy.clientWidth >= trackWidth - 20) {
+    stopRaceLine();
     message.textContent = `BẠN ĐÃ THUA!`;
     raceBtnElement.classList.add('disable');
     message.style.color = 'red';
@@ -34,9 +53,9 @@ function moveOpponent(enemy, currentPosition) {
 
 let enemy1Position = 0;
 let enemy2Position = 0;
-
+let raceInterval;
 function begin() {
-  let raceInterval = setInterval(() => {
+   raceInterval = setInterval(() => {
     if (gameWon) {
       return;
     }
@@ -52,14 +71,6 @@ function begin() {
 
 
 
-let gameStarted = false
-const finishLineElement = document.querySelector('.finish-line');
-const raceTrackElement = document.querySelector('.race-track');
-const message = document.querySelector('.message')
-const playerWidth = playerElement.clientWidth;
-const trackWidth = raceTrackElement.clientWidth;
-let gameWon = false;
-let currentPosition = 0;
 
 raceBtnElement.addEventListener('keydown', (e) => {
 
@@ -72,11 +83,13 @@ raceBtnElement.addEventListener('keydown', (e) => {
     if (gameWon) {
       return;
     }
+    startRaceLine();
     const randomStep = getRandomInt(10, 40);
     currentPosition += randomStep;
     playerElement.style.transform = `translateX(${currentPosition}px)`;
 
     if (10 + currentPosition + playerWidth >= trackWidth - 20) {
+      stopRaceLine();
       message.textContent = "BẠN ĐÃ THẮNG";
       raceBtnElement.classList.add('disable');
       gameWon = true;
@@ -86,27 +99,40 @@ raceBtnElement.addEventListener('keydown', (e) => {
     }
   }
 })
-raceBtnElement.addEventListener('click', (e) => {
-  
-    if (!gameStarted) {
-      begin();
-      gameStarted = true;
+raceBtnElement.addEventListener('keyup', (e) => {
+    if (e.key == "d" || e.key == "D") {
+        // Tắt animation ngay lập tức khi phím 'D' được nhả ra
+        if (!gameWon) {
+            stopRaceLine(); 
+        }
     }
-    const clickedBtn = e.currentTarget;
-    if (gameWon) {
-      return;
-    }
-    const randomStep = getRandomInt(10, 40);
-    currentPosition += randomStep;
-    playerElement.style.transform = `translateX(${currentPosition}px)`;
+});
 
-    if (10 + currentPosition + playerWidth >= trackWidth - 20) {
-      message.textContent = "BẠN ĐÃ THẮNG";
-      raceBtnElement.classList.add('disable');
-      gameWon = true;
-      const targetPosition = (trackWidth - 20) - playerWidth - 10;
-      currentPosition = targetPosition;
-      clearInterval(raceInterval);
-    }
+raceBtnElement.addEventListener('click', (e) => {
+  if (!gameStarted) {
+    begin();
+    gameStarted = true;
+  }
+  const clickedBtn = e.currentTarget;
+  if (gameWon) {
+    return;
+  }
+  startRaceLine();
+
+  const randomStep = getRandomInt(10, 40);
+  currentPosition += randomStep;
+  playerElement.style.transform = `translateX(${currentPosition}px)`;
+
+  if (10 + currentPosition + playerWidth >= trackWidth - 20) {
+    stopRaceLine();
+    message.textContent = "BẠN ĐÃ THẮNG";
+    raceBtnElement.classList.add('disable');
+    gameWon = true;
+    const targetPosition = (trackWidth - 20) - playerWidth - 10;
+    currentPosition = targetPosition;
+    clearInterval(raceInterval);
+  }
 })
+
+
 
